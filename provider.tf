785f0466dc24878fc5bac8872d04a703ad82ef3b
sanagareddy <53176627+sanagareddy@users.noreply.github.com>
2019-08-09 06:24:05 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "vault" {
+}
+
+data "vault_aws_access_credentials" "creds1" {
+  backend = "aws"
+  role = "RV1"
+}
+
+provider "aws" {
+  access_key = "${data.vault_aws_access_credentials.creds1.access_key}"
+  secret_key = "{data.vault_aws_access_credentails.creds1.secret_key}"
+  region = "ap-south-1"
+}
