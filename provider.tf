bc017cc22571facf7891f189558ed8349a6459f1
sanagareddy <sanagareddy@gmail.com>
2019-08-11 12:20:26 +0530

    git commit -m "upload"

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ data "vault_aws_access_credentials" "creds1" {
 
 provider "aws" {
   access_key = "${data.vault_aws_access_credentials.creds1.access_key}"
-  secret_key = "{data.vault_aws_access_credentails.creds1.secret_key}"
+  secret_key = "${data.vault_aws_access_credentails.creds1.secret_key}"
   region = "ap-south-1"
 }
