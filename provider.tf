21d2975486c46a159a569f8082f7c196281b4da5
sanagareddy <sanagareddy@gmail.com>
2019-08-11 16:05:14 +0530

    ok

--- provider.tf
+++ provider.tf
@@ -3,13 +3,13 @@ provider "vault" {
  token = "s.bBxHvihd2rnjyKGrYHbKjYWr"
 }
 
-data "vault_aws_access_credentials" "creds1" {
+data "vault_aws_access_credentials" "cred1" {
   backend = "aws"
   role = "Role1"
 }
 
 provider "aws" {
-  access_key = "${data.vault_aws_access_credentials.creds1.access_key}"
-  secret_key = "${data.vault_aws_access_credentials.creds1.secret_key}"
+  access_key = "${data.vault_aws_access_credentials.cred1.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.cred1.secret_key}"
   region = "ap-south-1"
 }
