10b53e9433911d5938e1bfcffb3d9a09218804e4
sanagareddy <sanagareddy@gmail.com>
2019-08-11 16:13:32 +0530

    adding subnets

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
   ami = "ami-0b99c7725b9484f9e"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.sub1.id}"
+  subnet_id = "${aws_subnet.pub_sub1.id}"
   tags = {
     "Name" = "Webserver1"
   }
@@ -12,7 +12,7 @@ resource "aws_vpc" "VPC1" {
   cidr_block = "10.0.0.0/24"
 }
 
-resource "aws_subnet" "sub1" {
+resource "aws_subnet" "pub_sub1" {
   availability_zone = "ap-south-1a"
   cidr_block = "10.0.0.0/25"
   vpc_id = "${aws_vpc.VPC1.id}"
